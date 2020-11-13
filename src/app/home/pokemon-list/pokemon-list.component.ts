c6d6189291dc5c328da14de8b79de972ada1449f
Daniel Heringer <danielheringer@tokenlab.com.br>
2020-11-13 10:53:27 -0300

    prettier

--- src/app/home/pokemon-list/pokemon-list.component.ts
+++ src/app/home/pokemon-list/pokemon-list.component.ts
@@ -10,10 +10,9 @@ import { PokemonType } from './../../models/PokemonTypes';
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.scss']
+  styleUrls: ['./pokemon-list.component.scss'],
 })
 export class PokemonListComponent implements OnInit, OnDestroy {
-
   @Input()
   allPokemons: Pokemon[];
 
@@ -28,109 +27,121 @@ export class PokemonListComponent implements OnInit, OnDestroy {
   searchOptions: SearchOptions;
   searchOptionsSubscription: Subscription;
 
-
   imgUrl = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/';
 
-  constructor(private pokemonListService: PokemonListService,
-          private navbarService: NavbarService) { }
+  constructor(
+    private pokemonListService: PokemonListService,
+    private navbarService: NavbarService
+  ) {}
 
   ngOnInit(): void {
-    this.searchOptionsSubscription = this.navbarService.getSearchOptions().subscribe((_searchOptions) =>{
-      this.searchOptions = _searchOptions;
-      this.searchPokemon();
-    })
-
+    this.searchOptionsSubscription = this.navbarService
+      .getSearchOptions()
+      .subscribe((_searchOptions) => {
+        this.searchOptions = _searchOptions;
+        this.searchPokemon();
+      });
   }
 
   onScroll(): void {
     this.loadMore();
-    if(this.offset < this.maxPokemonNumber && this.searchOptions.searchText === '' && this.searchOptions.searchType == pokemon_types.all){
+    if (
+      this.offset < this.maxPokemonNumber &&
+      this.searchOptions.searchText === '' &&
+      this.searchOptions.searchType == pokemon_types.all
+    ) {
       this.getPokemonList();
     }
   }
 
-  getPokemonList(){
+  getPokemonList() {
     this.loadingMore = true;
-    this.pokemonListService.getPokemonList(this.limit, this.offset).subscribe( pokemonList => {
-      for(let pokemon of pokemonList.results){
-        this.pokemonList.push(pokemon);
-      }
-      for(let pokemon of pokemonList.results){
-        this.pokemonImgLoading[pokemon.name] = true;
-      }
-      this.loadingMore = false;
-    });
+    this.pokemonListService
+      .getPokemonList(this.limit, this.offset)
+      .subscribe((pokemonList) => {
+        for (let pokemon of pokemonList.results) {
+          this.pokemonList.push(pokemon);
+        }
+        for (let pokemon of pokemonList.results) {
+          this.pokemonImgLoading[pokemon.name] = true;
+        }
+        this.loadingMore = false;
+      });
   }
 
-  searchPokemon(){
+  searchPokemon() {
     this.pokemonList = [];
-    if(this.searchOptions.searchText == '' && this.searchOptions.searchType == pokemon_types.all){
+    if (
+      this.searchOptions.searchText == '' &&
+      this.searchOptions.searchType == pokemon_types.all
+    ) {
       this.resetOffset();
       this.getPokemonList();
-    }
-    else{
-      if(this.searchOptions.searchType == pokemon_types.all){
-        this.pokemonListService.getAllPokemons().subscribe( allPokemons => {
-          for(let pokemon of allPokemons.results){
-            if(pokemon.name.search(this.searchOptions.searchText) != -1){
+    } else {
+      if (this.searchOptions.searchType == pokemon_types.all) {
+        this.pokemonListService.getAllPokemons().subscribe((allPokemons) => {
+          for (let pokemon of allPokemons.results) {
+            if (pokemon.name.search(this.searchOptions.searchText) != -1) {
               this.pokemonList.push(pokemon);
             }
           }
-        })
-      }
-      else{
-        this.pokemonListService.getPokemonsByType(this.searchOptions.searchType).subscribe( pokemonTypeRequest => {
-          let pokemonType = new PokemonType(pokemonTypeRequest);
-          for(let pokemon of pokemonType.pokemons){
-            if(pokemon.name.search(this.searchOptions.searchText) != -1){
-              if(this.getIdFromUrl(pokemon.url) < 893){
-                this.pokemonList.push(pokemon);
+        });
+      } else {
+        this.pokemonListService
+          .getPokemonsByType(this.searchOptions.searchType)
+          .subscribe((pokemonTypeRequest) => {
+            let pokemonType = new PokemonType(pokemonTypeRequest);
+            for (let pokemon of pokemonType.pokemons) {
+              if (pokemon.name.search(this.searchOptions.searchText) != -1) {
+                if (this.getIdFromUrl(pokemon.url) < 893) {
+                  this.pokemonList.push(pokemon);
+                }
               }
             }
-          }
-        })
+          });
       }
     }
   }
 
-  disableLoading(pokemonName: string){
-    this.pokemonImgLoading[pokemonName]=false
+  disableLoading(pokemonName: string) {
+    this.pokemonImgLoading[pokemonName] = false;
   }
 
-  getIdFromUrl(url: string): number{
+  getIdFromUrl(url: string): number {
     let splittedUrl = url.split('/');
-    let id = splittedUrl[splittedUrl.length-2];
+    let id = splittedUrl[splittedUrl.length - 2];
     return parseInt(id);
   }
 
-  getVisibility(pokemonName: string){
-    if(this.pokemonImgLoading[pokemonName]){
+  getVisibility(pokemonName: string) {
+    if (this.pokemonImgLoading[pokemonName]) {
       return 'collapse';
-    } else{
+    } else {
       return 'visible';
     }
   }
 
   zeroPad(num: number, places: number) {
     let zero = places - num.toString().length + 1;
-    return Array(+(zero > 0 && zero)).join("0") + num;
+    return Array(+(zero > 0 && zero)).join('0') + num;
   }
 
-  getImageUrl(pokemonUrl:string) {
+  getImageUrl(pokemonUrl: string) {
     const splittedUrl = pokemonUrl.split('/');
-    const pokemonNumber = parseInt(splittedUrl[splittedUrl.length-2]);
+    const pokemonNumber = parseInt(splittedUrl[splittedUrl.length - 2]);
     return this.imgUrl + this.zeroPad(pokemonNumber, 3) + '.png';
   }
 
-  loadMore(){
+  loadMore() {
     this.offset += this.limit; //adiciona limit ao offset
-    this.offset = this.offset > this.maxPokemonNumber ? this.maxPokemonNumber : this.offset;
-    if(this.offset+this.limit > this.maxPokemonNumber){
+    this.offset =
+      this.offset > this.maxPokemonNumber ? this.maxPokemonNumber : this.offset;
+    if (this.offset + this.limit > this.maxPokemonNumber) {
       this.limit = this.maxPokemonNumber - this.offset;
     }
   }
 
-  resetOffset(){
+  resetOffset() {
     this.offset = 0;
   }
 
--- src/app/home/pokemon-list/pokemon-list.service.ts
+++ src/app/home/pokemon-list/pokemon-list.service.ts
@@ -1,37 +1,44 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { PokemonList } from '../../models/Pokemon';
 import { PokemonType } from '../../models/PokemonTypes';
 import { pokemon_types } from 'src/app/utils/enums.utils';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonListService {
-
   baseUrl = 'https://pokeapi.co/api/v2/'; // api rest
   pokemons = [];
   maxPokemonNumber = 893;
 
   // injetando o HttpClient
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   // Headers
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  getPokemonList(limit: number, offset: number): Observable<PokemonList> {
+    return this.httpClient.get<PokemonList>(
+      this.baseUrl + 'pokemon?limit=' + limit + '&offset=' + offset
+    );
   }
 
-  getPokemonList(limit: number, offset: number): Observable<PokemonList>{
-    return this.httpClient.get<PokemonList>(this.baseUrl + 'pokemon?limit='+ limit + '&offset=' + offset);
+  getAllPokemons() {
+    return this.httpClient.get<PokemonList>(
+      this.baseUrl + 'pokemon?limit=' + this.maxPokemonNumber
+    );
   }
 
-  getAllPokemons(){
-    return this.httpClient.get<PokemonList>(this.baseUrl + 'pokemon?limit='+ this.maxPokemonNumber);
-  }
-
-  getPokemonsByType(type: pokemon_types): Observable<PokemonType>{
-    return this.httpClient.get<PokemonType>(this.baseUrl + 'type/'+ type);
+  getPokemonsByType(type: pokemon_types): Observable<PokemonType> {
+    return this.httpClient.get<PokemonType>(this.baseUrl + 'type/' + type);
   }
 
   // Manipulação de erros
@@ -42,10 +49,10 @@ export class PokemonListService {
       errorMessage = error.error.message;
     } else {
       // Erro ocorreu no lado do servidor
-      errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+      errorMessage =
+        `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
     }
     console.log(errorMessage);
     return throwError(errorMessage);
-  };
-
+  }
 }
